47c18322b94a79225120b6a2adcf53789d68075f
jessec <robertjessecooke@gmail.com>
2014-04-22 19:37:23 +0200

    turn off account page

--- src/app/account/account.js
+++ src/app/account/account.js
@@ -28,10 +28,10 @@ angular.module( 'core9Dashboard.account', [
 })
 
 .run(function(MenuService) {
-	MenuService.add('main', {title: "Account", weight: '1000', link: "account"});
-	MenuService.add('account', {title: "Delete", weight: '1000', link: "account"});
-	MenuService.add('account', {title: "Edit", weight: '1200', link: "account"});
-	MenuService.add('account', {title: "Back", weight: '1300', link: "account"});
+	//MenuService.add('main', {title: "Account", weight: '1000', link: "account"});
+	//MenuService.add('account', {title: "Delete", weight: '1000', link: "account"});
+	//MenuService.add('account', {title: "Edit", weight: '1200', link: "account"});
+	//MenuService.add('account', {title: "Back", weight: '1300', link: "account"});
 })
 
 ;
